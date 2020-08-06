b656a01a4b8afc625cd9de999cace2ef35ffd729
reemnx <37637982+reemnx@users.noreply.github.com>
2020-08-06 20:37:01 +0300

    Updates

---
+++ precache-manifest.d6a5518345aae70a0c78ed73c8397a77.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93ef17d7126cab202367b20fa5e2696",
+    "url": "/edea_souncloud/index.html"
+  },
+  {
+    "revision": "dc16388578ac927185da",
+    "url": "/edea_souncloud/static/css/main.2d097f07.chunk.css"
+  },
+  {
+    "revision": "e9edffa7e82bd88bbb8c",
+    "url": "/edea_souncloud/static/js/2.5cc4a8ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/edea_souncloud/static/js/2.5cc4a8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc16388578ac927185da",
+    "url": "/edea_souncloud/static/js/main.c1033535.chunk.js"
+  },
+  {
+    "revision": "f3fdbdaca093bcebdb41",
+    "url": "/edea_souncloud/static/js/runtime-main.c9fb009e.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/edea_souncloud/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "683362f36187ad8be18692df9c1cf81e",
+    "url": "/edea_souncloud/static/media/Muli-Medium.683362f3.ttf"
+  },
+  {
+    "revision": "3fc898075b85bba3248d2e735ffece52",
+    "url": "/edea_souncloud/static/media/down-arrow.3fc89807.svg"
+  },
+  {
+    "revision": "dbfe7e691e8cda611fcf249fafa9364a",
+    "url": "/edea_souncloud/static/media/download.dbfe7e69.svg"
+  },
+  {
+    "revision": "1b6009c18fa1377cb832f83d66e3d5b4",
+    "url": "/edea_souncloud/static/media/list.1b6009c1.svg"
+  },
+  {
+    "revision": "8039f2bc07ecf04c4ae06c7be11400e9",
+    "url": "/edea_souncloud/static/media/next.8039f2bc.svg"
+  },
+  {
+    "revision": "e4a05e4ac06b94d1923b251460a8093c",
+    "url": "/edea_souncloud/static/media/repeat.e4a05e4a.svg"
+  },
+  {
+    "revision": "37857ad3f846f65476cffb20d3dfc4e5",
+    "url": "/edea_souncloud/static/media/star.37857ad3.svg"
+  },
+  {
+    "revision": "8ae731c0010c6ccd8db6b4d385bf1372",
+    "url": "/edea_souncloud/static/media/tile.8ae731c0.svg"
+  }
+]);
